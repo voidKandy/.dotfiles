b6745b6888f5176dade7c59e1f162f975334247e
voidKandy <voidkandy@gmail.com>
2024-04-23 10:05:49 -0700

    transparent telescope & better tmux visuals

--- nvim/lua/custom/plugins/configs/colors.lua
+++ nvim/lua/custom/plugins/configs/colors.lua
@@ -23,6 +23,8 @@ return {
     vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
     vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
     vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
     -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
   end,
