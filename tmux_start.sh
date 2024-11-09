1e8cb03989298f19ebe81f33a1e4ceda3fb44a65
voidKandy <voidkandy@gmail.com>
2024-11-09 09:34:54 -0800

    added more to tmux start

--- tmux_start.sh
+++ tmux_start.sh
@@ -1,30 +1,83 @@
 #!/bin/zsh                                                                                                   
-
-SESSIONNAME="home"
-tmux has-session -t $HOME &> /dev/null
+source ~/.zshrc
+HOMESESSION="home"
+tmux has-session -t $HOMESESSION &> /dev/null
 
 if [ $? != 0 ] 
  then
-    tmux new-session -s $HOME -n obsidian -d
-    tmux send-keys -t $HOME:obsidian "source ~/.zshrc; obs;clear;nv Welcome.md" C-m 
+    tmux new-session -s $HOMESESSION -n obsidian -d
+    tmux send-keys -t $HOMESESSION:obsidian "obs;clear;nv Welcome.md" C-m 
 
-    tmux new-window -t $HOME -n dotfiles 
-    tmux send-keys -t $HOME:dotfiles "cd ~/.dotfiles;clear;tree" C-m 
+    tmux new-window -t $HOMESESSION -n dotfiles 
+    tmux send-keys -t $HOMESESSION:dotfiles "cd ~/.dotfiles;clear;tree -L 2" C-m 
 
-    tmux new-window -t $HOME -n desktop
-    tmux send-keys -t $HOME:desktop "cd ~/Desktop;clear" C-m 
+    tmux new-window -t $HOMESESSION -n desktop
+    tmux send-keys -t $HOMESESSION:desktop "cd ~/Desktop;clear" C-m 
 
-    tmux new-window -t $HOME -n downloads
-    tmux send-keys -t $HOME:downloads "cd ~/Downloads;clear" C-m 
+    tmux new-window -t $HOMESESSION -n downloads
+    tmux send-keys -t $HOMESESSION:downloads "cd ~/Downloads;clear" C-m 
 
-    tmux new-window -t $HOME -n projects
-    tmux send-keys -t $HOME:projects "cd ~/Documents/projects;clear;nvim" C-m 
+    tmux new-window -t $HOMESESSION -n projects
+    tmux send-keys -t $HOMESESSION:projects "proj;clear;nvim" C-m 
 
-    tmux new-window -t $HOME -n zsh
-    tmux send-keys -t $HOME:zsh "cd ~/Documents/projects;clear;ls" C-m 
+    tmux new-window -t $HOMESESSION -n zsh
+    tmux send-keys -t $HOMESESSION:zsh "proj;clear;ls" C-m 
 
-    tmux select-window -t $HOME:obsidian
+    tmux select-window -t $HOMESESSION:obsidian
 fi
 
-tmux attach -t $HOME
+ESPX="espx"
+tmux has-session -t $ESPX &> /dev/null
+
+if [ $? != 0 ] 
+ then
+    tmux new-session -s $ESPX -n 'lsp-zsh' -d
+    tmux send-keys -t $ESPX:'lsp-zsh' "proj;cd espx-ls;clear;tree -L 2" C-m 
+
+    tmux new-window -t $ESPX -n 'lsp-nvim'
+    tmux send-keys -t $ESPX:'lsp-nvim' "proj;cd espx-ls;clear;nvim" C-m 
+
+    tmux new-window -t $ESPX -n 'lsp-testing'
+    tmux send-keys -t $ESPX:'lsp-testing' "proj;cd espx-ls/testing;clear;tree -L 2" C-m 
+
+    tmux new-window -t $ESPX -n 'lib-nvim'
+    tmux send-keys -t $ESPX:'lib-nvim' "proj;cd espionox;clear;nvim" C-m 
+
+    tmux new-window -t $ESPX -n 'lib-zsh'
+    tmux send-keys -t $ESPX:'lib-zsh' "proj;cd espionox;clear;tree -L 2" C-m 
+
+
+    tmux select-window -t $ESPX:'lsp-zsh'
+fi
+
+
+PRATTL="prattl"
+tmux has-session -t $PRATTL &> /dev/null
+# prattl, prattl-web, go-pyenv
+
+if [ $? != 0 ] 
+ then
+    tmux new-session -s $PRATTL -n zsh -d
+    tmux send-keys -t $PRATTL:zsh "proj;cd prattl;clear;tree -L 2" C-m 
+
+    tmux new-window -t $PRATTL -n nvim
+    tmux send-keys -t $PRATTL:nvim "proj;cd prattl;clear;nvim" C-m 
+
+    tmux new-window -t $PRATTL -n 'go-pyenv-zsh' 
+    tmux send-keys -t $PRATTL:'go-pyenv-zsh' "proj;cd go-pyenv;clear;tree -L 2" C-m 
+
+    tmux new-window -t $PRATTL -n 'go-pyenv-nvim'
+    tmux send-keys -t $PRATTL:'go-pyenv-nvim' "proj;cd go-pyenv;clear;nvim" C-m 
+
+    tmux new-window -t $PRATTL -n 'web-zsh' 
+    tmux send-keys -t $PRATTL:'web-zsh' "proj;cd prattl-web;clear;tree -L 2" C-m 
+
+    tmux new-window -t $PRATTL -n 'web-nvim'
+    tmux send-keys -t $PRATTL:'web-nvim' "proj;cd prattl-web;clear;nvim" C-m 
+
+
+    tmux select-window -t $PRATTL:zsh
+fi
+
+tmux attach -t $HOMESESSION
 
