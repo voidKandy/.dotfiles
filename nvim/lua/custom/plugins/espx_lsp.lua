9fe08dd6eb2affd3e78abb01c60726f709aeaf97
voidKandy <voidkandy@gmail.com>
2024-12-26 16:54:35 -0800

    added ghostty

--- nvim/lua/custom/plugins/espx_lsp.lua
+++ nvim/lua/custom/plugins/espx_lsp.lua
@@ -8,9 +8,10 @@ if not configs.espx_ls then
       name = 'espx-ls',
       autostart = true,
       cmd = { 'espx-ls' },
-      filetypes = {
-        "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue",
-        "svelte", 'text', "sh", 'rust', 'javascript', "html", "go", "htmldjango" },
+      -- filetypes = {
+      --   "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue",
+      --   "svelte", 'text', "sh", 'rust', 'javascript', "html", "go", "htmldjango" },
+      filetypes = {},
       root_dir = function()
         return vim.fs.dirname(vim.fs.find({ '.espx' }, { upward = true })[1])
       end
